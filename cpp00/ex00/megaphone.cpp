93637fb8cc819b5ac3b2eacae758e5b3d5a2d6bf
msgenan <msgenan07@gmail.com>
2025-11-21 15:42:11 +0300

    style: minor change with syntax

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
+#include <iostream> // std::cout, std::endl
+#include <cctype>   // std::toupper
 
 int main(int argc, char **argv)
 {
     if(argc < 2)
+    {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
     for(int i = 1; i < argc; i++)
-      for(int j = 0; argv[i][j]; j++)
+    {
+        for(int j = 0; argv[i][j]; j++)
             std::cout << (char)std::toupper(argv[i][j]);
+    }
     std::cout << std::endl;
+    return (0);
 }
