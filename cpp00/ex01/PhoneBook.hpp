7887ce53ffd0ac0ac6c94d67d0b3cf307c6c6886
msgenan <msgenan07@gmail.com>
2025-11-27 19:41:13 +0300

    feat: phonebook class is defined

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -2,8 +2,23 @@
 # define PHONEBOOK_HPP
 
 #include <string>
+#include "Contact.hpp"
 
+class PhoneBook
+{
+    private:
+        Contact contacts[8];
+        int     contactCount;
+        int     nextContact;
 
+        std::string formatField(const std::string &str) const;
 
+    public:
+        PhoneBook();
+
+        void addContact();
+        void searchContacts() const;
+        void displayContact(int index) const;
+};
 
 #endif
