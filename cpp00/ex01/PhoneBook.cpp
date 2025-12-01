1cf4af97074079cedeffc5acb7f88f36f3598bc6
msgenan <msgenan07@gmail.com>
2025-12-01 14:30:55 +0300

    feat: searchcontacts added

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -34,3 +34,51 @@ void PhoneBook::addContact()
         contactCount++;
     contactIndex = (contactIndex + 1) % 8;
 }
+
+std::string PhoneBook::formatField(const std::string &str) const
+{
+    if (str.length() > 10)
+        return str.substr(0, 9) + ".";
+    else
+        return str;
+}
+
+void PhoneBook::searchContacts() const
+{
+    std::cout << std::setw(10) << "Index"      << "|"
+              << std::setw(10) << "First Name" << "|"
+              << std::setw(10) << "Last Name"  << "|"
+              << std::setw(10) << "Nickname"   << "|"
+              << std::endl;
+    for (int i = 0; i < contactCount; i++)
+    {
+        std::cout << std::setw(10) << i << "|"
+            << std::setw(10) << formatField(contacts[i].getFirstName()) << "|"
+            << std::setw(10) << formatField(contacts[i].getLastName())  << "|"
+            << std::setw(10) << formatField(contacts[i].getNickname())  << "|"
+            << std::endl;
+    }
+}
+
+int main()
+{
+    PhoneBook phonebook;
+    std::string command;
+
+    while (true)
+    {
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+        std::getline(std::cin, command);
+
+        if (command == "ADD")
+            phonebook.addContact();
+        else if (command == "SEARCH")
+            phonebook.searchContacts();
+        else if (command == "EXIT")
+            break;
+        else
+            std::cout << "Unknown command." << std::endl;
+    }
+
+    return 0;
+}
