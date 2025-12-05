0d6bf2b0df44330bf3958b83831b3963c7f6fbd1
msgenan <msgenan07@gmail.com>
2025-12-05 16:50:52 +0300

    chore: minor change at if block

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
     if(argc < 2)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        return (0);
     }
     for(int i = 1; i < argc; i++)
     {
