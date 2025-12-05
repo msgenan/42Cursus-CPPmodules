0f17cd2e2e56e66fe288063034abbc993dbfda44
msgenan <msgenan07@gmail.com>
2025-12-05 16:53:42 +0300

    chore: change deleted

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,6 @@ int main(int argc, char **argv)
     if(argc < 2)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-        return (0);
     }
     for(int i = 1; i < argc; i++)
     {
