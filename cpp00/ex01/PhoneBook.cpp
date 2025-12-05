83ec2b726f1ce2be222a769b25e1f91c31bf4f63
msgenan <msgenan07@gmail.com>
2025-12-05 17:12:37 +0300

    refactor: main function moved into the main.cpp

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,4 +1,7 @@
 #include "PhoneBook.hpp"
+#include <iostream> // std::cout, std::cin, std::endl, std::getline
+#include <iomanip>  // std::setw
+#include <cctype>   // isdigit
 
 PhoneBook::PhoneBook()
 {
@@ -118,29 +121,4 @@ int PhoneBook::getValidIndex() const
         return -1;
     }
     return index;
-}
-
-int main()
-{
-    PhoneBook phonebook;
-    std::string command;
-
-    while (true)
-    {
-        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        std::getline(std::cin, command);
-
-        if (std::cin.eof())
-            break;
-
-        if (command == "ADD")
-            phonebook.addContact();
-        else if (command == "SEARCH")
-            phonebook.searchContacts();
-        else if (command == "EXIT")
-            break;
-        else
-            std::cout << "Unknown command." << std::endl;
-    }
-    return 0;
 }
