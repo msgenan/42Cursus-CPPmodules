617e61388b2a86a801015aa064f219a6b52391b8
msgenan <msgenan07@gmail.com>
2025-12-22 17:17:02 +0300

    style: minor change at printing messages

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,25 +6,31 @@ Harl::~Harl() {}
 void Harl::debug(void)
 {
     std::cout << BOLD << BLUE << "[ DEBUG ]" << RESET << std::endl;
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. "
+              << "I really do!" << std::endl << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout << BOLD << GREEN << "[ INFO ]" << RESET << std::endl;
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl << std::endl;
+    std::cout << BOLD << GREEN << "[ INFO ]" << RESET << std::endl
+              << "I cannot believe adding extra bacon costs more money. "
+              << "\nYou didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+              << std::endl << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << BOLD << YELLOW << "[ WARNING ]" << RESET << std::endl;
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl << std::endl;
+    std::cout << BOLD << YELLOW << "[ WARNING ]" << RESET << std::endl
+              << "I think I deserve to have some extra bacon for free. "
+              << "\nI've been coming for years, whereas you started working here just last month."
+              << std::endl << std::endl;
 }
 
 void Harl::error(void)
 {
-    std::cout << BOLD << RED << "[ ERROR ]" << RESET << std::endl;
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
+    std::cout << BOLD << RED << "[ ERROR ]" << RESET << std::endl
+              << "This is unacceptable! I want to speak to the manager now.\n"
+              << std::endl;
 }
 
 void Harl::complain(std::string level)
