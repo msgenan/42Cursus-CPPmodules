c64dec2aacf3e9bbe85068a5a681f3950af2275c
msgenan <msgenan07@gmail.com>
2025-12-23 15:54:24 +0300

    style: added color codes

--- cpp01/ex03/HumanA.hpp
+++ cpp01/ex03/HumanA.hpp
@@ -8,6 +8,9 @@
 #define BOLD   "\033[1m"
 #define RED    "\033[31m"
 #define GREEN  "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE   "\033[34m"
+#define CYAN   "\033[36m"
 
 class HumanA {
 private:
--- cpp01/ex04/replace.hpp
+++ cpp01/ex04/replace.hpp
@@ -10,6 +10,7 @@
 #define RED    "\033[31m"
 #define GREEN  "\033[32m"
 #define YELLOW "\033[33m"
+#define BLUE   "\033[34m"
 #define CYAN   "\033[36m"
 
 #endif
