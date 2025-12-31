0089c33c2b35f44cf5d258acc46c14139021fdcd
msgenan <msgenan07@gmail.com>
2025-12-31 23:06:51 +0300

    style: syntax changed or improved

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -10,19 +10,11 @@ class Fixed
         static const int    _fractionalBits = 8;
 
     public:
-        // 1. Default Constructor
         Fixed();
-
-        // 2. Copy Constructor
         Fixed(const Fixed& other);
-
-        // 3. Copy Assignment Operator Overload
         Fixed& operator=(const Fixed& other);
-
-        // 4. Destructor
         ~Fixed();
 
-        // Member Functions
         int     getRawBits(void) const;
         void    setRawBits(int const raw);
 };
--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) {
-    // 1. Default Constructor çağrılır. _fixedPointValue = 0 olur.
+int main( void )
+{
     Fixed a;
-
-    // 2. Copy Constructor çağrılır. 'a' nesnesi 'b'ye kopyalanır.
-    Fixed b( a );
-
-    // 3. Default Constructor çağrılır. 'c' nesnesi oluşturulur.
+    Fixed b(a);
     Fixed c;
-
-    // 4. Copy Assignment Operator çağrılır. 'b'nin değerleri 'c'ye aktarılır.
     c = b;
 
-    // getRawBits çağrıları ve değerlerin yazdırılması
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
 
     return 0;
-    // Program biterken a, b ve c için Destructor'lar ters sırayla çağrılır.
 }
