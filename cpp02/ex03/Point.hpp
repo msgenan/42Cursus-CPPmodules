8dffc99a4163d9b766c1781500f8146728a0161c
msgenan <msgenan07@gmail.com>
2026-01-03 21:29:14 +0300

    style: add color codes

--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -3,6 +3,14 @@
 
 # include "Fixed.hpp"
 
+#define RESET  "\033[0m"
+#define BOLD   "\033[1m"
+#define RED    "\033[31m"
+#define GREEN  "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE   "\033[34m"
+#define CYAN   "\033[36m"
+
 class Point
 {
 private:
