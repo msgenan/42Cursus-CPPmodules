db12f630e78734633702ce3e2b3e6ab252e660b9
msgenan <msgenan07@gmail.com>
2026-01-03 22:24:12 +0300

    chore: files created

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,10 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : _name("default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap default constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+}
