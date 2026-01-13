28d5593de9e332dc4ee2c04fcc94005f123f44e6
msgenan <msgenan07@gmail.com>
2026-01-13 19:07:22 +0300

    feat: add DiamondTrap with multiple inheritance for ex03

---
+++ cpp03/ex03/DiamondTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+class DiamondTrap : public FragTrap, public ScavTrap
+{
+private:
+    std::string _name;
+
+public:
+    /* --- Orthodox Canonical Form --- */
+    DiamondTrap();
+    DiamondTrap(const std::string& name);
+    DiamondTrap(const DiamondTrap& other);
+    DiamondTrap& operator=(const DiamondTrap& other);
+    ~DiamondTrap();
+
+    /* --- Member Functions --- */
+    void attack(const std::string& target);
+    void whoAmI(void);
+};
+
+#endif
