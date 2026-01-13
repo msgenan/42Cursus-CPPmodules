1701530dd1cb480333ea1024b48d21b7ef5ad4cc
msgenan <msgenan07@gmail.com>
2026-01-13 19:05:32 +0300

    Add FragTrap header file for ex02

---
+++ cpp03/ex02/FragTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+public:
+    /* --- Orthodox Canonical Form --- */
+    FragTrap();
+    FragTrap(const std::string& name);
+    FragTrap(const FragTrap& other);
+    FragTrap& operator=(const FragTrap& other);
+    ~FragTrap();
+
+    /* --- Member Functions --- */
+    void attack(const std::string& target);
+    void highFivesGuys(void);
+};
+
+#endif
