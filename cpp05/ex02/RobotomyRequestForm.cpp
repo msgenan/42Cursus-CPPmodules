93848c36851daee26c9410751370522435ed767e
msgenan <msgenan07@gmail.com>
2026-04-07 16:57:03 +0300

    feat: Robotomy class added

---
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,36 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() 
+    : AForm("RobotomyRequestForm", 72, 45), _target("default_target") {}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string& target) 
+    : AForm("RobotomyRequestForm", 72, 45), _target(target) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& other) 
+    : AForm(other), _target(other._target) {}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm& other) 
+{
+    if (this != &other)
+    {
+        this->_target = other._target;
+    }
+    return *this;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+
+void RobotomyRequestForm::executeAction() const 
+{
+    std::cout << "* Bzzzzzztt... Drrrrrrrrr... (Intense drilling noises) *" << std::endl;
+
+    if (rand() % 2 == 0)
+    {
+        std::cout << this->_target << " has been robotomized successfully!" << std::endl;
+    }
+    else
+    {
+        std::cout << "The robotomy on " << this->_target << " failed." << std::endl;
+    }
+}
