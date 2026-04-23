3a45811cc48958082507481cdab85b3be7548422
msgenan <msgenan07@gmail.com>
2026-04-23 21:50:27 +0300

    feat: easyfind function added

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <exception>
+# include <iterator>
+
+class ValueNotFoundException : public std::exception {
+public:
+    virtual const char* what() const throw() {
+        return "Error: Missing value!";
+    }
+};
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value) {
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end()) {
+        throw ValueNotFoundException();
+    }
+    return it;
+}
+
+#endif
