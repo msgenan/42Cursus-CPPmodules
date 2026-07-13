4fc10838ebb0153cbd6864fb616be84688babd55
msgenan <msgenan07@gmail.com>
2026-07-13 17:45:40 +0300

    feat: add new library and temp func

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,18 +3,23 @@
 
 #include <map>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 class BitcoinExchange
 {
 private:
     std::map<std::string, float> _data;
+
 public:
     BitcoinExchange();
     BitcoinExchange(const BitcoinExchange &other);
     BitcoinExchange &operator=(const BitcoinExchange &other);
     ~BitcoinExchange();
+
+    void ParseData();
+    void PrintData() const; // TEMPORARY DEBUG - remove before submission
 };
-
-
 
 #endif
