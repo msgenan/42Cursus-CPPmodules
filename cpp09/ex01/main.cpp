395cf0352b6cc593509bb9d81ae97e2d2ab225e4
msgenan <msgenan07@gmail.com>
2026-07-14 17:47:38 +0300

    feat: add executable main

---
+++ cpp09/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "RPN.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+
+    RPN rpn;
+    try
+    {
+        int result = rpn.PolishAlgorithm(argv[1]);
+        std::cout << result << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
