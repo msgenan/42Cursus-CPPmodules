ca4de0287773cf11a2b22f7451a78708ff21e69d
msgenan <msgenan07@gmail.com>
2026-07-14 17:48:55 +0300

    feat: add helper funcs

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -34,3 +34,41 @@ int RPN::PolishAlgorithm(const std::string &input)
     return _stack.top();
 }
 
+void RPN::applyOperator(const std::string &token)
+{
+    int right; // en son push edilen (sağdaki operand)
+    int left;  // ondan önce push edilen (soldaki operand)
+
+    right = _stack.top();
+    _stack.pop();
+    left = _stack.top();
+    _stack.pop();
+
+    if (token[0] == '+')
+        _stack.push(left + right);
+    else if (token[0] == '-')
+        _stack.push(left - right);
+    else if (token[0] == '*')
+        _stack.push(left * right);
+    else if (token[0] == '/')
+    {
+        if (right == 0)
+            throw std::runtime_error("Error");
+        _stack.push(left / right);
+    }
+}
+
+bool RPN::isNumber(const std::string &token) const
+{
+    if (token.size() == 1 && std::isdigit(static_cast<unsigned char>(token[0])))
+        return true;
+    return false;
+}
+
+bool RPN::isOperator(const std::string &token) const
+{
+    if (token.size() == 1 && (token[0] == '+' || token[0] == '-'
+            || token[0] == '*' || token[0] == '/'))
+        return true;
+    return false;
+}
